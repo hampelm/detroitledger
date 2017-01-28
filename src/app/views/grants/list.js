c86ba28542be52652d027d87b6ad3bfe07204686
Benjamin Chodoroff <bc@thermitic.net>
2017-01-27 20:18:33 -0500

    move logic to collection

--- src/app/views/grants/list.js
+++ src/app/views/grants/list.js
@@ -12,7 +12,7 @@ var GrantListView = Backbone.View.extend({
   template: template,
 
   initialize: function(options) {
-    _.bindAll(this, 'prep', 'group', 'render');
+    _.bindAll(this, 'prep', 'render');
 
     this.direction = options.direction;
     this.$el = options.$parent || $(options.el);
@@ -31,68 +31,8 @@ var GrantListView = Backbone.View.extend({
     };
   },
 
-  group: function(grant) {
-    if(this.direction === 'received') {
-      return grant.field_funder.target_id;
-    }
-    return grant.field_recipient.target_id;
-  },
-
-  //group_names[grant.field_recipient.target_id] = grant.field_recipient.name;
-  // Group each of the grants by granter or grantee
   prep: function() {
-    var grantsJSON = this.collection.toJSON();
-    var byOrganizationID = _.groupBy(grantsJSON, this.group);
-    var group_names_by_id = {};
-    _.each(grantsJSON, function(g) {
-      group_names_by_id[g.field_funder.target_id] = g.field_funder.name;
-      group_names_by_id[g.field_recipient.target_id] = g.field_recipient.name;
-    });
-
-    // Add counts etc.
-    var readyData = [];
-    var yearly_sums = {};
-    var grant_tags = {};
-    _.each(byOrganizationID, function(grants, organziation_id) {
-      var sum = _.reduce(grants, function(memo, grant) {
-        // along the way, build our yearly sums & grant tags.
-        var this_year = grant.field_year.value.slice(0,4);
-        if (yearly_sums[this_year] > 0) {
-          yearly_sums[this_year] += grant.field_funded_amount;
-        } else {
-          yearly_sums[this_year] = grant.field_funded_amount;
-        }
-        if (grant.field_grant_tags && grant.field_grant_tags.hasOwnProperty('und')) {
-          _.forEach(grant.field_grant_tags.und, function(grant_tag) {
-            grant_tags[grant_tag.tid] = {
-              name: grant_tag.name,
-              id: grant_tag.tid,
-              count: grant_tags[grant_tag.tid] ? grant_tags[grant_tag.tid].count + 1 : 1,
-            };
-          });
-        }
-        return memo + grant.field_funded_amount;
-      }, 0);
-
-      readyData.push({
-        sum: sum,
-        prettySum: numeral(sum).format('0,0[.]00'),
-        id: organziation_id,
-        name: group_names_by_id[organziation_id],
-        slug: util.slugify(group_names_by_id[organziation_id]),
-        grants: grants
-      });
-    });
-
-    readyData = _.sortBy(readyData, function(organization) {
-      return organization.sum;
-    }).reverse();
-
-    return {
-      organizations: readyData,
-      yearly_sums: yearly_sums,
-      grant_tags: _.reverse(_.sortBy(_.values(grant_tags), 'count'))
-    };
+    return this.collection.prepForGrantList(this.direction);
   },
 
   render: function() {
